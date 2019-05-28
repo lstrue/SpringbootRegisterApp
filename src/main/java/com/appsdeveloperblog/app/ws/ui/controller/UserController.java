47f32155262beb6e2130cb6a05bdfeb2299c1720
David Li <shuai_li@neimanmarcus.com>
2019-05-28 15:30:19 -0500

    Exception handle and debug

--- src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
+++ src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
@@ -101,6 +101,10 @@ public class UserController {
 			MediaType.APPLICATION_XML_VALUE,
 			MediaType.APPLICATION_JSON_VALUE})
 	public ResponseEntity<UserRest> getUser(@PathVariable String userId) {
+		
+		String firstName = null;
+		int firstNameLength = firstName.length();
+		
 		if(users.containsKey(userId)) {
 			return new ResponseEntity<>(users.get(userId), HttpStatus.OK);
 		}else {
@@ -147,8 +151,9 @@ public class UserController {
 		return storeUserDetails;
 	}
 	
-	@DeleteMapping
-	public String deleteUser() {
-		return "delete user called";
+	@DeleteMapping(path="/{id}")
+	public ResponseEntity<Void> deleteUser(@PathVariable String id) {
+		users.remove(id);
+		return ResponseEntity.noContent().build();
 	}	
 }
