ce0a57f6c768075b767ad15eea80d2fabef24636
David Li <shuai_li@neimanmarcus.com>
2019-05-28 14:21:07 -0500

    Create method in controller

--- src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
+++ src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
@@ -1,15 +1,19 @@
 package com.appsdeveloperblog.app.ws.ui.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.appsdeveloperblog.app.ws.ui.model.request.UserDetailsRequestModel;
 import com.appsdeveloperblog.app.ws.ui.model.response.UserRest;
 
 @RestController
@@ -35,20 +39,50 @@ public class UserController {
 //		return returnValue;
 //	}
 	
+//	@GetMapping(path="/{userId}", produces = { 
+//			MediaType.APPLICATION_XML_VALUE,
+//			MediaType.APPLICATION_JSON_VALUE})
+//	public UserRest getUser(@PathVariable String userId) {
+//		UserRest returnValue = new UserRest();
+//		returnValue.setEmail("test@test.com");
+//		returnValue.setFirstName("David");
+//		returnValue.setLastName("Lee");
+//		return returnValue;
+//	}
+	
 	@GetMapping(path="/{userId}", produces = { 
 			MediaType.APPLICATION_XML_VALUE,
 			MediaType.APPLICATION_JSON_VALUE})
-	public UserRest getUser(@PathVariable String userId) {
+	public ResponseEntity<UserRest> getUser(@PathVariable String userId) {
 		UserRest returnValue = new UserRest();
 		returnValue.setEmail("test@test.com");
 		returnValue.setFirstName("David");
 		returnValue.setLastName("Lee");
-		return returnValue;
+		return new ResponseEntity<UserRest>(returnValue, HttpStatus.OK);
+//		return new ResponseEntity<UserRest>(HttpStatus.BAD_REQUEST);
 	}
 	
-	@PostMapping
-	public String createUser() {
-		return "create user called";
+	//create
+	//http://localhost:8080/users
+	//{
+//	"firstName": "Melinda",
+//	"lastName": "Gates",
+//	"email": "bill@ms.com",
+//	"password": "123"
+//}
+	//accept Content-Type application/json 
+	@PostMapping(consumes = { 
+				MediaType.APPLICATION_XML_VALUE,
+				MediaType.APPLICATION_JSON_VALUE},
+			produces = { 
+				MediaType.APPLICATION_XML_VALUE,
+				MediaType.APPLICATION_JSON_VALUE})
+	public ResponseEntity createUser(@RequestBody UserDetailsRequestModel userDetails) {
+		UserRest returnValue = new UserRest();
+		returnValue.setEmail(userDetails.getEmail());
+		returnValue.setFirstName(userDetails.getFirstName());
+		returnValue.setLastName(userDetails.getLastName());
+		return new ResponseEntity<UserRest>(returnValue, HttpStatus.OK);
 	}
 	
 	@PutMapping
