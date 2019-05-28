6e70aff7d081d9132a478fb3cb2a3bf9a27fb545
David Li <shuai_li@neimanmarcus.com>
2019-05-28 11:09:16 -0500

    Controller PathVariable RequestParam and Model

--- src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
+++ src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
@@ -2,18 +2,36 @@ package com.appsdeveloperblog.app.ws.ui.controller;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.appsdeveloperblog.app.ws.ui.model.response.UserRest;
+
 @RestController
-@RequestMapping("users")
+@RequestMapping("/users")
 public class UserController {
 
+//	@GetMapping(path="/{userId}")
+//	public String getUser(@PathVariable String userId) {
+	
 	@GetMapping
-	public String getUser() {
-		return "get user called";
+	public String getUser(@RequestParam(value="page", defaultValue="1") int page,
+			@RequestParam(value="limit", defaultValue="50") int limit,
+			@RequestParam(value="sort",  defaultValue="desc", required = false) String sort) {
+		return "get user called page = " + page + " limit " + limit + " sort: " + sort;
+	}
+	
+	@GetMapping(path="/{userId}")
+	public UserRest getUser(@PathVariable String userId) {
+		UserRest returnValue = new UserRest();
+		returnValue.setEmail("test@test.com");
+		returnValue.setFirstName("David");
+		returnValue.setLastName("Lee");
+		return returnValue;
 	}
 	
 	@PostMapping
